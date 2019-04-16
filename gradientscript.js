6b90c0a5997954e084a2277a0e7cb24798646cf5
mitbuiviet <mitbuiviet@gmail.com>
2019-04-16 21:18:54 +0900

    Update gradientscript.js

--- gradientscript.js
+++ gradientscript.js
@@ -94,7 +94,7 @@ for (var k=0; k< leftchoice.length; k++){
 
 function colorrefresh(){
 document.body.classList.remove("blackhole");
-document.styleSheets[1].cssRules[0].style.background="linear-gradient(217deg,"+color1.value+",rgba(255,0,0,0)70.71%),linear-gradient(127deg,#C8DBAC  ,rgba(0,255,0,0) 70.71%),linear-gradient(336deg,"+color2.value+",rgba(0,0,255,0)70.71%)";
+document.styleSheets[1].cssRules[0].style.background="linear-gradient(217deg,"+color1.value+",rgba(255,0,0,0)70.71%),linear-gradient(127deg,#C8DBAC  ,rgba(0,255,0,0) 70.71%),linear-gradient(336deg,rgba(0,0,255,0),"+color2.value+"70.71%)";
 document.styleSheets[1].cssRules[0].style.backgroundSize="400% 400%";
 document.styleSheets[1].cssRules[0].style.webkitAnimation="Gradient 6s ease infinite;";
 document.styleSheets[1].cssRules[0].style.animation="Gradient 6s ease infinite;";
